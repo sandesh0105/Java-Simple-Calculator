3dc6538c8217a972473123a2c45bad7fa5da5523
Sanpai69 <f20230092@hyderabad.bits-pilani.ac.in>
2024-10-30 22:08:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package SimpleCalc;
+
+public class Main {
+    public static void main(String[] args) {
+       CalcFrame cal = new CalcFrame();
+       
+       // get text store in file and then new line character 
+    }
+}
